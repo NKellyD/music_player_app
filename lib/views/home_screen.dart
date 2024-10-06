1c398f7906f965cbb7bfac2cb8cde3f98aeacb81
Niyuhire Kelly Derlin <niyuhirekderlin@gmail.com>
2024-10-06 12:35:47 +0200

    CheckAAudioQuery

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -7,7 +8,6 @@ import 'package:on_audio_query/on_audio_query.dart';
 import '../const/colors.dart';
 import '../const/text_style.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -15,68 +15,94 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var controller = Get.put(PlayerController());
     return Scaffold(
-      backgroundColor: bgDarkColor,
-      appBar: AppBar(
         backgroundColor: bgDarkColor,
-        leading: const Icon(Icons.sort_rounded,color: whiteColor,),
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search,color: whiteColor,),),
-        ],
-        title:  const Text('Beats',
-          style:TextStyle(
+        appBar: AppBar(
+          backgroundColor: bgDarkColor,
+          leading: const Icon(
+            Icons.sort_rounded,
+            color: whiteColor,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: whiteColor,
+              ),
+            ),
+          ],
+          title: const Text(
+            'Beats',
+            style: TextStyle(
               color: Colors.white,
               fontSize: 28,
               fontFamily: 'Calibri',
-            fontWeight: FontWeight.bold,
-          ),),
-
-      ),
-      body: FutureBuilder<List<SongModel>>(
-          future: controller.audioQuery.querySongs(
-            ignoreCase: true,
-            orderType: OrderType.ASC_OR_SMALLER,
-            sortType: null,
-            uriType: UriType.EXTERNAL,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-          builder: (BuildContext context, snapshot){
-
-          }
-      )
-    );
+        ),
+        body: FutureBuilder<List<SongModel>>(
+            future: controller.audioQuery.querySongs(
+              ignoreCase: true,
+              orderType: OrderType.ASC_OR_SMALLER,
+              sortType: null,
+              uriType: UriType.EXTERNAL,
+            ),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.data == null) {
+                if (kDebugMode) {
+                  print(snapshot.data);
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.data!.isEmpty) {
+                return const Text(
+                  'No Song Found',
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                );
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: 100,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 4),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                            tileColor: bgColor,
+                            title: const Text(
+                              'Music name',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontFamily: 'Calibri'),
+                            ),
+                            subtitle: const Text(
+                              'Artist name',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                  fontFamily: 'Calibri'),
+                            ),
+                            leading: const Icon(
+                              Icons.music_note,
+                              color: whiteColor,
+                              size: 32,
+                            ),
+                            trailing: const Icon(
+                              Icons.play_arrow,
+                              color: whiteColor,
+                              size: 26,
+                            ),
+                          ),
+                        );
+                      }),
+                );
+              }
+            }));
   }
 }
-
-// Padding(
-// padding: const EdgeInsets.all(8.0),
-// child: ListView.builder(
-// physics: const BouncingScrollPhysics(),
-// itemCount: 100,
-// itemBuilder: (BuildContext context, int index){
-// return Container(
-// margin: const EdgeInsets.only(bottom: 4),
-// child:   ListTile(
-// shape: RoundedRectangleBorder(
-// borderRadius: BorderRadius.circular(12)
-// ),
-// tileColor: bgColor,
-// title: Text(
-// 'Music name',
-// style: TextStyle(
-// color: Colors.white,
-// fontSize: 15,
-// fontFamily: 'Calibri'
-// ),),
-// subtitle: Text(
-// 'Artist name',
-// style: TextStyle(
-// color: Colors.white,
-// fontSize: 12,
-// fontFamily: 'Calibri'
-// ),),
-// leading: Icon(Icons.music_note,color: whiteColor,size: 32,),
-// trailing: Icon(Icons.play_arrow,color: whiteColor,size: 26,),
-// ),
-// );
-// }
-// ),
-// )
