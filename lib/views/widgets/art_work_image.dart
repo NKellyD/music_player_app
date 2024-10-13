b03db6b1bdf26bfb101737ccd9402ee3468535db
Niyuhire Kelly Derlin <niyuhirekderlin@gmail.com>
2024-10-13 10:17:50 +0200

    add artworkimage widget

---
+++ lib/views/widgets/art_work_image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ArtWorkImage extends StatelessWidget {
+  const ArtWorkImage({super.key,required this.image});
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
